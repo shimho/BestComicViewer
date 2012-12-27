61bbce7b05bb39be6335f7cbdc205932b06f50e0
shimho <shimho@gmail.com>
2012-12-26 16:48:21 -0800

    change

--- gen/com/example/bestcomicviewer/R.java
+++ gen/com/example/bestcomicviewer/R.java
@@ -22,20 +22,30 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int black_overlay=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int famous_dave=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int left_arrow=0x7f020002;
+        public static final int right_arrow=0x7f020003;
     }
     public static final class id {
         public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int label=0x7f070006;
+        public static final int label_main_layout=0x7f070003;
+        public static final int label_scroll=0x7f070005;
+        public static final int left_arrow=0x7f070004;
+        public static final int right_arrow=0x7f070007;
     }
     public static final class layout {
         public static final int activity_comic_main=0x7f030000;
+        public static final int activity_comic_view=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050002;
         public static final int dummy_content=0x7f050001;
+        public static final int famous_dave_img_desc=0x7f050003;
     }
     public static final class style {
         /** 
